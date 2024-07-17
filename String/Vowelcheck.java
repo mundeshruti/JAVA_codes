e49c9c5e2f3a612fd1cc23290e9ca23b2996f97a
mundeshrutika <mundeshrutika7@gmail.com>
2024-07-17 22:58:34 +0530

    Update Vowelcheck.java

--- String/Vowelcheck.java
+++ String/Vowelcheck.java
@@ -1,18 +1,26 @@
 package String;
 
 public class Vowelcheck {
-    public static void main(String[] args) {
-        String s = "shrutika";
-        char ch;
+     public static void main(String[] args) {
+        String s = "SHrutikA";
         int count = 0;
+        int countc = 0;
+        char ch;
         for (int i = 0; i < s.length(); i++) {
             ch = s.charAt(i);
-            if ((ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u')) {
-                System.out.println(ch);
+            if ((ch == 'a') || (ch == 'e') || (ch == 'i') || (ch == 'o') || (ch == 'u') || (ch == 'A') || (ch == 'E')
+                    || (ch == 'I') || (ch == 'O') || (ch == 'U')) {
                 count++;
+                System.out.println("vowel:" + ch);
+
+            } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                System.out.println("consonent:" + ch);
+                countc++;
+
             }
         }
-        System.out.println("total no. of vowel:" + count);
+        System.out.println("No.of vowels:" + count);
+        System.out.println("No.of consonent:" + countc);
 
     }
 
