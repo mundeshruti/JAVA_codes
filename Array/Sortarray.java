382984de958390839ff3adc484864091c3fa9110
mundeshrutika <mundeshrutika7@gmail.com>
2024-07-18 00:43:55 +0530

    Update Sortarray.java

--- Array/Sortarray.java
+++ Array/Sortarray.java
@@ -16,7 +16,7 @@ public class Sortarray {
 
         // int arr[] = { 1, 4, 6, 9, 0, 4, 3 };
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
+            for (int j = i ; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     int temp = arr[i];
                     arr[i] = arr[j];
