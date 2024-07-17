b11d15a1aef25d8c93e1713d6ae97b65ee8e3a4d
mundeshrutika <mundeshrutika7@gmail.com>
2024-07-17 22:59:12 +0530

    Update ReverseSentence.java

--- String/ReverseSentence.java
+++ String/ReverseSentence.java
@@ -3,18 +3,14 @@ package String;
 import java.util.Scanner;
 
 public class ReverseSentence {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a sentence:");
-        String s = sc.nextLine();
-        // String s = "My Name is shrutika";
-        String[] r = s.split(" ");
-        String rev = "";
-
-        for (int i = r.length - 1; i >= 0; i--) {
-            rev += r[i] + " ";
+     public static void main(String[] args) {
+        String s = "My Name is Shrutika";
+        String r = "";
+        String rev[] = s.split(" ");
+        for (int i = 0; i < r.length(); i++) {
+            s = rev[i] + r;
         }
+        System.out.println(s);
 
-        System.out.println("Reversed sentence: " + rev);
     }
 }
