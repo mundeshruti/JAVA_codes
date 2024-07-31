63b9282d1c87a31876eb404127022c7f58d7608c
mundeshrutika <108685550+mundeshruti@users.noreply.github.com>
2024-07-31 19:16:49 +0530

    commit

---
+++ Pattern/Empty_Triangle.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+public class Empty_Triangle {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                if (k == 0 || k == i || i == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
